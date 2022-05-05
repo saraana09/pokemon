7db84759434e42864e360a94404412d9e9309e1e
saraana09 <3.pengyou.000577@gmail.com>
2022-05-05 14:53:08 -0400

    Connected my database, can see json in the browser

---
+++ models/pokemon.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const pokemons = [
+    {name: "bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur"},
+    {name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur"},
+    {name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur"},
+    {name: "charmander", img: "http://img.pokemondb.net/artwork/charmander"},
+    {name: "charizard", img: "http://img.pokemondb.net/artwork/charizard"},
+    {name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle"},
+    {name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle"}
+ ];
+
+const pokemon = express.Router()
+
+pokemon.route('/')
+ .get((req, res) => {
+    res.status(200).json(pokemons)
+ })
+
+ module.exports = pokemon
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const morgan =  require("morgan");
+const pokemon = require("./models/pokemon")
 
 require("dotenv").config();
 
 const server = express();
 const PORT = process.env.PORT || 3000;
+
+server.use(express.json())
+server.use("/pokemon", pokemon)
 
 server.get("/", (req, res)=>{
     res.status(200).json({message: "Welcome to the Pokemon App"})
